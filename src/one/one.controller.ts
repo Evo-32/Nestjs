33f42155bca54c175ffd8dfbef9dd930133756d8
Evo-32 <evodieumuhuza@gmail.com>
2026-08-17 20:46:22 +0200

    addingn the dto

--- src/one/one.controller.ts
+++ src/one/one.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Query, Body } from '@nestjs/common';
+import { CreateStudentDto } from './dto/create-student.dto';
+import { UpdateStudentDto } from './dto/update-student.dto';
 
 @Controller('one')
-export class OneController {}
+export class OneController {
+  // GET names
+  @Get()
+  getStudent(@Query('major') major: string){
+    return [
+        {major}
+    ]
+  }
+
+  //GET by id
+  @Get(':id')
+  getOneStudent(@Param('id') id: string){
+    return {
+        id
+    }
+  }
+
+
+  //POST names
+  @Post()
+  createStudent(@Body() createStudentDto:CreateStudentDto){
+    return {
+        name: createStudentDto.name
+    }
+  }
+
+
+  //PUT by id
+  @Put(':id')
+  createOneStudent(@Param('id') id: string, @Body() UpdateStudentDto:UpdateStudentDto){
+    return {
+        id,
+        name: UpdateStudentDto
+    }
+  }
+
+
+  //DELETE by id
+  @Delete(':id')
+  removeStudent(@Param('id') id: string){
+    return {}
+  }
+}
